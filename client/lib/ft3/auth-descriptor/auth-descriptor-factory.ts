c67367fda14e11960494bffd1588c4f31757924d
Joso Marković <joso.markovic@chromaway.com>
2019-10-03 12:26:24 +0200

    Added 'id' property to AuthDescriptor interface

--- client/lib/ft3/auth-descriptor/auth-descriptor-factory.ts
+++ client/lib/ft3/auth-descriptor/auth-descriptor-factory.ts
@@ -1,6 +1,6 @@
 import { gtv } from 'postchain-client';
-import {AuthDescriptor} from "../account";
-import SingleSignatureAuthDescriptor from "./single-signature-auth-descriptor";
+import {AuthDescriptor} from '../account';
+import SingleSignatureAuthDescriptor from './single-signature-auth-descriptor';
 
 export default class AuthDescriptorFactory {
     create(type: string, args: Buffer): AuthDescriptor {
