9abc217333765aa452537836f3a4ce033db195cb
rsibani <riccardo.sibani@chromaway.com>
2020-01-28 17:20:13 +0100

    making ft3-lib independent from testnet chain

---
+++ rell/restart-testnet.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eux
+
+echo "HELLO! Setting up the dev chain"
+rm -rf ./target
+
+./postchain-node/postchain.sh wipe-db -nc testnet-config/node-config.properties
+
+./postchain-node/multigen.sh testnet-config/run.xml -d src -o target/
+
+BRID=`cat ./target/blockchains/0/brid.txt`
+echo $BRID
+
+exec ./postchain-node/postchain.sh run-node-auto -d target
