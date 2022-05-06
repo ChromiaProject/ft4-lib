f8b7fafffb77e6fc2ffc4f87ea3c592dd62fb985
mainuser <francesco.santanche@chromaway.com>
2022-05-06 10:28:03 +0100

    made tests use iid. made run.xml create two chains, for test and test_admin

--- test/util/blockchain-util.ts
+++ test/util/blockchain-util.ts
@@ -1,13 +1,11 @@
 import Blockchain from "../../client/lib/ft3/core/blockchain/blockchain";
+import Postchain from "../../client/lib/ft3/core/postchain";
 import DirectoryServiceUtil from "./directory-service-util";
 
 require('dotenv').config();
 
 export default class BlockchainUtil {
-    static async getDefaultBlockchain(): Promise<Blockchain> {
-        return await Blockchain.initialize(
-            Buffer.from(process.env.CHAIN_ID, 'hex'),
-            DirectoryServiceUtil.getDefaultDirectoryService()
-        )
-    }
+	static async getDefaultBlockchain(): Promise<Blockchain> {
+		return await new Postchain(process.env.NODE_URL).blockchain(0);
+	}
 }
