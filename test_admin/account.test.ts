18504648ae635d9307b5d33e64de058cbe595682
mainuser <francesco.santanche@chromaway.com>
2022-05-18 12:25:44 +0100

    removed duplicate tests

--- test_admin/account.test.ts
+++ test_admin/account.test.ts
@@ -31,17 +31,6 @@ describe('Test the account', () => {
         blockchain = await BlockchainUtil.getDefaultBlockchain();
     });
 
-    it('should be in DEV mode', () => {
-        expect(process.env.DEV).toBe('true');
-    });
-
-    it("Correctly creates keypair", () => {
-        const keyPairToImport = pcl.util.makeKeyPair();
-        const user = new KeyPair(buffToHex(keyPairToImport.privKey));
-        expect(user.privKey).toEqual(keyPairToImport.privKey);
-        expect(user.pubKey).toEqual(keyPairToImport.pubKey);
-    });
-
     it("Register account on blockchain", async () => {
         const user = TestUser.singleSig();
         const authDescriptor = new SingleSignatureAuthDescriptor(
@@ -70,21 +59,6 @@ describe('Test the account', () => {
         expect(account.authDescriptor.length).toBe(2);
     });
 
-    it("cannot add new auth descriptor if account doesn't have account edit rights", async () => {
-        const user = TestUser.singleSig();
-        const account = await Account.register(
-            new SingleSignatureAuthDescriptor(user.keyPair.pubKey, [FlagsType.Transfer]),
-            blockchain.newSession(user)
-        );
-        expect(account).not.toBeNull();
-
-        const promise = account.addAuthDescriptor(
-            new SingleSignatureAuthDescriptor(user.keyPair.pubKey, [FlagsType.Transfer])
-        );
-        await expect(promise).rejects.toBeInstanceOf(Error);
-        expect(account.authDescriptor.length).toBe(1);
-    });
-
     it("should create new multisig account", async () => {
         const user1 = TestUser.singleSig();
         const user2 = TestUser.singleSig();
@@ -105,27 +79,6 @@ describe('Test the account', () => {
         const account = await Account.getById(authDescriptor.id, blockchain.newSession(user1));
 
         expect(account.id).toEqual(authDescriptor.id);
-    });
-
-    //TODO FIX ME
-    it.skip("should update account if 2 signatures provided", async () => {
-        const user1 = TestUser.singleSig();
-        const user2 = TestUser.singleSig();
-
-        const account = await Account.register(
-            new MultiSignatureAuthDescriptor(
-                [user1.keyPair.pubKey, user2.keyPair.pubKey],
-                2,
-                [FlagsType.Account, FlagsType.Transfer]
-            ),
-            blockchain.newSession(user1)
-        );
-        expect(account).not.toBeNull();
-
-        await account.addAuthDescriptor(
-            new SingleSignatureAuthDescriptor(user1.keyPair.pubKey, [FlagsType.Transfer])
-        );
-        expect(account.authDescriptor.length).toBe(2);
     });
 
     it("should fail if only one signature provided", async () => {
@@ -161,19 +114,6 @@ describe('Test the account', () => {
         expect(account.authDescriptor.length).toBe(1);
     });
 
-    it("should be returned when queried by participant id", async () => {
-        const user = TestUser.singleSig();
-
-        await AccountBuilder
-            .account(blockchain, user)
-            .withParticipants([user.keyPair])
-            .build();
-
-        const accounts = await Account.getByParticipantId(user.keyPair.pubKey, blockchain.newSession(user));
-
-        expect(accounts.length).toEqual(1);
-    });
-
     it("should return two accounts when account is participant of two accounts", async () => {
         const user1 = TestUser.singleSig();
         const user2 = TestUser.singleSig();
@@ -201,18 +141,6 @@ describe('Test the account', () => {
         const accounts = await Account.getByParticipantId(user1.keyPair.pubKey, blockchain.newSession(user1));
 
         expect(accounts.length).toEqual(2);
-    });
-
-    it('should return account by id', async () => {
-        const user = TestUser.singleSig();
-
-        const account = await AccountBuilder
-            .account(blockchain, user)
-            .build();
-
-        const foundAccount = await Account.getById(account.id_, blockchain.newSession(user));
-
-        expect(account).toEqual(foundAccount);
     });
 
     it('should have only main auth descriptor left after calling deleteAllAuthDescriptorsExclude with main auth_descriptor', async () => {
