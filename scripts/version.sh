e6a3c5a66a201dda2b82792ddfa646081824d11b
FrancescoSnt <francesco.santanche@chromaway.com>
2022-08-22 14:59:31 +0100

    enhanced versioning script

--- scripts/version.sh
+++ scripts/version.sh
@@ -2,21 +2,11 @@
 
 updaterell(){
     version=$1
-    echo $version | grep -qe "c$"
-    if test $? -eq 0 
-    then
-      echo "ERR: You passed a client version number to the -r argument"
-      exit 1
-    fi
-    echo $version | grep -qe "r$"
-    if test $? -eq 0 
-    then
+    if [[ $version =~ r$ ]]; then
       version=${version::-1}
     fi
-    echo $version | grep -qE "[0-9]+\.[0-9]+\.[0-9]+$"
-    if test $? -eq 1 
-    then
-      echo "ERR: Invalid version format"
+    if ! [[ $version =~ [0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+      echo "ERROR: Invalid version format"
       exit 1
     fi
     sed -i 's/[0-9]\+\.[0-9]\+\.[0-9]\+r/'${version}'r/' rell/src/lib/ft3/version.rell
@@ -26,10 +16,13 @@ updaterell(){
 while getopts 'h' OPTION; do
   case "$OPTION" in
     h)
-        echo "If you want to update the rell version, use \n\tnpm run version -r [version number]\n"
+        echo "If you want to update the rell version, use \n\tnpm run version [version number] | major | minor | patch\n"
         echo "The letter at the end of the version is optional.\n\n"
-        echo "Example:\n\n\tnpm run version -r 4.1.3"
+        echo "Example:\n\n\tnpm run version 4.1.3"
         echo "Will update the rell version to 4.1.3r"
+        echo "\n\n\tnpm run version major"
+        echo "Will update the rell version from 4.1.3r to 5.0.0r"
+        echo "\nMinor and patch will update the rell version from 4.1.3r to 4.2.0r and 4.1.4r, respectively"
         exit 0
         ;;
     ?)
@@ -40,5 +33,33 @@ while getopts 'h' OPTION; do
 done
 shift "$(($OPTIND -1))"
 
-echo "moving rell version to $1";
-updaterell $1
+input=$1;
+pattern="[0-9]+\.[0-9]+\.[0-9]+r?$";
+
+if [[ "$input" =~ $pattern ]]; then
+  version="$input";
+  echo "matched"
+else
+  currVersion=$(grep -Po "[0-9\.]+(?=r)" rell/src/lib/ft3/version.rell);
+  if [[ "$input" == "major" ]]; then
+    major=$(echo $currVersion | grep -Po "^[0-9]+");
+    version="$((major+1)).0.0";
+  elif [[ "$input" == "minor" ]]; then
+    major=$(echo $currVersion | grep -Po "^[0-9]+");
+    minor=$(echo $currVersion | grep -Po "(?<=\.)[0-9]+(?=\.)");
+    version="$major.$((minor+1)).0";
+  elif [[ "$input" == "patch" ]]; then
+    major=$(echo $currVersion | grep -Po "^[0-9]+");
+    minor=$(echo $currVersion | grep -Po "(?<=\.)[0-9]+(?=\.)");
+    patch=$(echo $currVersion | grep -Po "(?<=\.)[0-9]+$");
+    version="$major.$minor.$((patch+1))";
+  else
+    echo "Unrecognized option: '$input'. It should be one of major | minor | patch | version number"
+    echo "Version number should be formatted as [digits].[digits].[digits], with an optional r at the end"
+    echo "If you're trying to edit the client version (as in 4.2.5c), use 'npm version'"
+    exit 1
+  fi
+fi
+
+echo "moving rell version to $version";
+updaterell "$version"
