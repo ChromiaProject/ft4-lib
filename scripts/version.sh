96e116549e5dc3b15b6a6c80c4a3ac176ef96221
FrancescoSnt <francesco.santanche@chromaway.com>
2022-10-06 10:27:36 +0100

    fixed formatting

--- scripts/version.sh
+++ scripts/version.sh
@@ -16,17 +16,17 @@ updaterell(){
 while getopts 'h' OPTION; do
   case "$OPTION" in
     h)
-        echo -e "If you want to update the rell version, use \n\tnpm run version [version number] | major | minor | patch\n"
+        echo -e "If you want to update the rell version, use \n\tnpm run version:rell [version number] | major | minor | patch\n"
         echo -e "The letter at the end of the version is optional.\n\n"
-        echo -e "Example:\n\n\tnpm run version 4.1.3"
+        echo -e "Example:\n\n\tnpm run version:rell 4.1.3"
         echo -e "Will update the rell version to 4.1.3r"
-        echo -e "\n\n\tnpm run version major"
+        echo -e "\n\n\tnpm run version:rell major"
         echo -e "Will update the rell version from 4.1.3r to 5.0.0r"
         echo -e "\nMinor and patch will update the rell version from 4.1.3r to 4.2.0r and 4.1.4r, respectively"
         exit 0
         ;;
     ?)
-        echo "script usage: npm run version [-- -h] [-r 'version']" >&2
+        echo "script usage: npm run version:rell [-- -h] [-r 'version']" >&2
         exit 1
         ;;
   esac
@@ -56,6 +56,7 @@ else
     echo "Unrecognized option: '$input'. It should be one of major | minor | patch | version number"
     echo "Version number should be formatted as [digits].[digits].[digits], with an optional r at the end"
     echo "If you're trying to edit the client version (as in 4.2.5c), use 'npm version'"
+    echo "For further help, use 'npm run version:rell -- -h'"
     exit 1
   fi
 fi
