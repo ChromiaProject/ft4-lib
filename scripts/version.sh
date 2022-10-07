ed09ca1f173bf6691ce9a0cf208cf34d1dd46be4
FrancescoSnt <francesco.santanche@chromaway.com>
2022-10-07 10:55:20 +0100

    slimmed down versioning script

--- scripts/version.sh
+++ scripts/version.sh
@@ -13,53 +13,85 @@ updaterell(){
     sed -i 's/toEqual("[0-9]\+\.[0-9]\+\.[0-9]\+r")/toEqual("'${version}'r")/' test/blockchain.test.ts
 }
 
-while getopts 'h' OPTION; do
-  case "$OPTION" in
+function usage {
+  echo -e "~~~~~~~~~~~~~~~~~~~HELP~~~~~~~~~~~~~~~~~~~"
+  echo -e "\nIf you want to update the rell version, use"
+  echo -e "\n|\tnpm run version:rell [-- -Mmp] | [version number]\n"
+  echo -e "The 'r' at the end of the version is optional.\n\n"
+  echo -e "Examples:\n\n|\tnpm run version:rell 4.1.3"
+  echo -e "|Will update the rell version to 4.1.3r"
+  echo -e "\n\n|\tnpm run version:rell -- -M"
+  echo -e "|Will update the rell version from 4.1.3r to 5.0.0r"
+  echo -e "\nMinor (-m) and patch (-p) will update the rell version from 4.1.3r to 4.2.0r and 4.1.4r, respectively."
+  echo -e "\n\n|\tnpm run version:rell -- -mp"
+  echo -e "|Will update the rell version from 4.1.3r to 4.2.1r"
+  echo -e "\n\nIf a version number is passed after flags, it is discarded:"
+  echo -e "\n|\tnpm run version:rell -- -p 15.1.3r"
+  echo -e "|Will update the rell version from 4.1.3r to 4.1.4r"
+}
+
+# Increment a version string using Semantic Versioning (SemVer) terminology.
+
+# Parse command line options.
+
+no_args=true
+major=false
+minor=false
+patch=false
+while getopts "hMmp" Option
+do
+  case $Option in
+    M ) major=true;;
+    m ) minor=true;;
+    p ) patch=true;;
     h)
-        echo -e "If you want to update the rell version, use \n\tnpm run version:rell [version number] | major | minor | patch\n"
-        echo -e "The letter at the end of the version is optional.\n\n"
-        echo -e "Example:\n\n\tnpm run version:rell 4.1.3"
-        echo -e "Will update the rell version to 4.1.3r"
-        echo -e "\n\n\tnpm run version:rell major"
-        echo -e "Will update the rell version from 4.1.3r to 5.0.0r"
-        echo -e "\nMinor and patch will update the rell version from 4.1.3r to 4.2.0r and 4.1.4r, respectively"
-        exit 0
+      usage
+      exit 0
         ;;
-    ?)
-        echo "script usage: npm run version:rell [-- -h] [-r 'version']" >&2
-        exit 1
-        ;;
+    \?)
+      echo "usage: $(basename $0) [-- -hMmp] [major.minor.patch]" >&2
+      exit 1
+      ;;
   esac
+  no_args=false
 done
-shift "$(($OPTIND -1))"
 
-input=$1;
-pattern="[0-9]+\.[0-9]+\.[0-9]+r?$";
+shift $(($OPTIND - 1))
 
-if [[ "$input" =~ $pattern ]]; then
-  version="$input";
-else
-  currVersion=$(grep -o "[0-9]*\.[0-9]*\.[0-9]*" rell/src/lib/ft3/version.rell);
-  if [[ "$input" == "major" ]]; then
-    major=$(echo $currVersion | sed -n 's/\([0-9][0-9]*\)\..*/\1/p');
-    version="$((major+1)).0.0";
-  elif [[ "$input" == "minor" ]]; then
-    major=$(echo $currVersion | sed -n 's/\([0-9][0-9]*\)\..*/\1/p');
-    minor=$(echo $currVersion | sed -n 's/.*\.\([0-9][0-9]*\)\..*/\1/p');
-    version="$major.$((minor+1)).0";
-  elif [[ "$input" == "patch" ]]; then
-    major=$(echo $currVersion | sed -n 's/\([0-9][0-9]*\)\..*/\1/p');
-    minor=$(echo $currVersion | sed -n 's/.*\.\([0-9][0-9]*\)\..*/\1/p');
-    patch=$(echo $currVersion | sed -n 's/.*\.\([0-9][0-9]*\)$/\1/p');
-    version="$major.$minor.$((patch+1))";
-  else
-    echo "Unrecognized option: '$input'. It should be one of major | minor | patch | version number"
-    echo "Version number should be formatted as [digits].[digits].[digits], with an optional r at the end"
-    echo "If you're trying to edit the client version (as in 4.2.5c), use 'npm version'"
-    echo "For further help, use 'npm run version:rell -- -h'"
-    exit 1
-  fi
+currVersion=$(grep -o "[0-9]*\.[0-9]*\.[0-9]*" rell/src/lib/ft3/version.rell);
+if [[ $no_args = "true" ]]; then version=$1; else version=$currVersion; fi
+
+# Build array from version string.
+
+a=( ${version//./ } )
+
+# If version string is missing or has the wrong number of members, show usage message.
+
+if [ ${#a[@]} -ne 3 ]
+then
+  echo "usage: $(basename $0) [-- -hMmp] [major.minor.patch]"
+  exit 1
 fi
 
-echo "moving rell version to $version";
-updaterell "$version"
+# Increment version numbers as requested.
+
+if $major;
+then
+  ((a[0]++))
+  a[1]=0
+  a[2]=0
+fi
+
+if $minor;
+then
+  ((a[1]++))
+  a[2]=0
+fi
+
+if $patch;
+then
+  ((a[2]++))
+fi
+
+echo -e "moving rell version to ${a[0]}.${a[1]}.${a[2]}";
+updaterell "${a[0]}.${a[1]}.${a[2]}"
