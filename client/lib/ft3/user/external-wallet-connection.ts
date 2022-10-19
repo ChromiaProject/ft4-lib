00fbb28e6401a5de68ead321cf9001262dcf2836
Linus Lagerhjelm <linus.lagerhjelm@omegapoint.se>
2022-10-19 10:35:59 +0200

    Move event handlers into constructor

--- client/lib/ft3/user/external-wallet-connection.ts
+++ client/lib/ft3/user/external-wallet-connection.ts
@@ -1,46 +1,44 @@
 import WalletConnect from "@walletconnect/client";
 
 interface ConnectionCallbackParams {
-  accounts: string[],
-  chainId: number,
+  accounts: string[];
+  chainId: number;
 }
 
 const BRIDGE_URL = "https://bridge.walletconnect.org";
 export default class ExternalWalletConnection {
-  private _accounts: string[] = []
-  private _chainId = 0
+  private _accounts: string[] = [];
+  private _chainId = 0;
 
-  private constructor(private connector: WalletConnect) {}
+  private constructor(private connector: WalletConnect) {
+    connector.on("connect", this.updateSession.bind(this));
+    connector.on("session_update", this.updateSession.bind(this));
+    connector.on("disconnect", this.disconnectSession.bind(this));
+  }
 
   public static async init(): Promise<ExternalWalletConnection> {
     const connector = await ExternalWalletConnection.createConnection();
-    const conn = new ExternalWalletConnection(connector);
-    
-    connector.on("connect", conn.updateSession.bind(conn))
-    connector.on("session_update", conn.updateSession.bind(conn))
-    connector.on("disconnect", conn.disconnectSession.bind(conn))
-
-    return conn
+    return new ExternalWalletConnection(connector);
   }
 
   private updateSession(error: Error, params: ConnectionCallbackParams) {
     if (error) {
       throw error;
     }
-    this._accounts = params.accounts
-    this._chainId = params.chainId
+    this._accounts = params.accounts;
+    this._chainId = params.chainId;
   }
-  
-  private disconnectSession(error: Error, params: ConnectionCallbackParams) {
-    this.connector = undefined
+
+  private disconnectSession() {
+    this.connector = undefined;
   }
 
   private async ensureConnection() {
     if (!this.connector) {
       this.connector = await ExternalWalletConnection.createConnection();
-      this.connector.on("connect", this.updateSession.bind(this))
-      this.connector.on("session_update", this.updateSession.bind(this))
-      this.connector.on("disconnect", this.disconnectSession.bind(this))
+      this.connector.on("connect", this.updateSession.bind(this));
+      this.connector.on("session_update", this.updateSession.bind(this));
+      this.connector.on("disconnect", this.disconnectSession.bind(this));
     }
   }
 
@@ -50,29 +48,32 @@ export default class ExternalWalletConnection {
     });
 
     await connector.createSession();
-    return connector
+    return connector;
   }
 
   get connectionUri(): string {
-    return this.connector.uri
+    return this.connector.uri;
   }
 
   get accounts(): string[] {
-    return [...this._accounts]
+    return [...this._accounts];
   }
 
   get chainId(): number {
-    return this._chainId
+    return this._chainId;
   }
 
-  async signMessage({ message, account }: { message: string, account: string }): Promise<string> {
-    await this.ensureConnection()
+  async signMessage({
+    message,
+    account,
+  }: {
+    message: string;
+    account: string;
+  }): Promise<string> {
+    await this.ensureConnection();
 
-    const msgParams = [
-      message, 
-      account,
-    ];
+    const msgParams = [message, account];
 
-    return this.connector.signPersonalMessage(msgParams)
+    return this.connector.signPersonalMessage(msgParams);
   }
 }
