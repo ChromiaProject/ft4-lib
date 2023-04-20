6ef270e9198251bf309717fcf37661cee582a92a
Joso Marković <joso.markovic@chromaway.com>
2023-04-20 16:33:49 +0200

    FT3-80 - Use smart transaction builder in account builder

--- client/lib/ft3/asset/mappers.ts
+++
@@ -1,9 +0,0 @@
-import { Balance } from "./types";
-
-export function mapBalance(balance: any): Balance {
-  const { id, name, brid, amount } = balance;
-  return Object.freeze({
-    asset: { id, name, brid },
-    amount,
-  });
-}
