43e5c7da044fc0939ead02af0c9abc3ae07ce97c
Linus Lagerhjelm <linus.lagerhjelm@omegapoint.se>
2023-06-05 16:04:38 +0200

    Spring clean dependencies

--- client/lib/ft3/authentication/evm/index.ts
+++ client/lib/ft3/authentication/evm/index.ts
@@ -35,7 +35,7 @@ export async function signMessage(
   signer: ethers.Signer
 ): Promise<Signature> {
   const signature = await signer.signMessage(message);
-  const { r, s, v } = ethers.utils.splitSignature(signature);
+  const { r, s, v } = ethers.Signature.from(signature);
   return {
     r: Buffer.from(r.slice(2), "hex"),
     s: Buffer.from(s.slice(2), "hex"),
