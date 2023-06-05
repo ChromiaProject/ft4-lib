b34f74ddfc437658c0cf68e3cef4f20dc42e196a
FrancescoSnt <francesco.santanche@chromaway.com>
2023-06-05 13:03:49 +0100

    reversed name change

--- scripts/jesttest.sh
+++ scripts/jesttest.sh
@@ -78,7 +78,7 @@ mkdir logs
 if $docker; then
     docker run --name ft4_jest_test -e POSTGRES_INITDB_ARGS="--lc-collate=C.UTF-8 \
         --lc-ctype=C.UTF-8 --encoding=UTF-8" -e POSTGRES_USER=postchain \
-        --tmpfs=/pgtmpfs:size=1000m -e PGDATA=/pgtmpfs -e POSTGRES_DB=postchain_test \
+        --tmpfs=/pgtmpfs:size=1000m -e PGDATA=/pgtmpfs -e POSTGRES_DB=postchain \
         -e POSTGRES_PASSWORD=postchain -p 5432:5432 -d postgres > ./logs/postgres.log;
 fi
 
