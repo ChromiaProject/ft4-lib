dce404b05be91d65c5da515b502d446771c83066
Konstantin Alexandrov <konstantin.alexandrov@kindly.ai>
2023-06-28 11:16:07 +0200

    Rename account->accounts and core->assets

---
+++ client/lib/ft3/accounts/account/index.ts
@@ -0,0 +1,126 @@
+import { GtxClient } from "postchain-client/built/src/gtx/interfaces";
+import { BufferId } from "../../cryptoUtils";
+import { legacyTransactionBuilder } from "../utils/transaction-builder-old";
+import {
+  addAuthDescriptorToAccount,
+  burnTokens,
+  deleteAllAuthDescriptorsExclude,
+  deleteAuthDescriptor,
+  getPaymentHistoryIterator,
+  givePoints,
+  registerAccount,
+  ssoRawTransactionAddAuthDescriptor,
+  ssoRawTransactionRegister,
+  transfer,
+  xcTransfer,
+} from "./account-op-functions";
+import {
+  getByAuthDescriptorId,
+  getById,
+  getByIds,
+  getByParticipantId,
+  getRateLimit,
+  isAuthDescriptorValid,
+} from "./account-query-functions";
+import { AuthDescriptor } from "./auth-descriptor/types";
+import { ensurePaymentHistoryStoreLocal } from "./payment-history/payment-history-store-local";
+import { createPaymentHistoryStoreMemory } from "./payment-history/payment-history-store-memory";
+import { User } from "./types";
+import { deriveAccountId, toGtv } from "./auth-descriptor";
+import { Amount } from "../asset/interfaces";
+import { PaymentHistoryFilter } from "./payment-history/types";
+
+export * from "./auth";
+export * from "./auth-descriptor";
+export * from "./payment-history";
+export * from "./types";
+
+export const accountQuerySession = (pci: GtxClient) =>
+  Object.freeze({
+    by: {
+      participantId: (id: BufferId) => getByParticipantId(pci, id),
+      authDescriptorId: (id: BufferId) => getByAuthDescriptorId(pci, id),
+      ids: (ids: BufferId[]) => getByIds(pci, ids),
+      id: (id: BufferId) => getById(pci, id),
+    },
+    paymentHistory: {
+      iterator: getPaymentHistoryIterator,
+      storeMemory: (
+        accountId: BufferId,
+        pageSize: number,
+        filter: PaymentHistoryFilter | null = null
+      ) => createPaymentHistoryStoreMemory(pci, accountId, pageSize, filter),
+      storeLocal: (
+        accountId: BufferId,
+        pageSize: number,
+        filter: PaymentHistoryFilter | null = null
+      ) => ensurePaymentHistoryStoreLocal(pci, pageSize, accountId, filter),
+    },
+    isAuthDescriptorValid: (accountId: BufferId, authDescriptorId: BufferId) =>
+      isAuthDescriptorValid(pci, accountId, authDescriptorId),
+    rateLimit: (accountId: BufferId) => getRateLimit(pci, accountId),
+    idFromAuthDescriptor: (firstAuthDescriptor: AuthDescriptor) =>
+      deriveAccountId(toGtv(firstAuthDescriptor)),
+  });
+
+export const accountUserSession = (user: User, pci: GtxClient) =>
+  Object.freeze({
+    sso: {
+      ssoRegister: (authDescriptor: AuthDescriptor) =>
+        ssoRawTransactionRegister(
+          authDescriptor,
+          user.authDescriptor,
+          legacyTransactionBuilder(user, pci)
+        ),
+      ssoAddAuthDescriptor: (
+        accountId: BufferId,
+        authDescriptor: AuthDescriptor
+      ) =>
+        ssoRawTransactionAddAuthDescriptor(
+          accountId,
+          authDescriptor,
+          legacyTransactionBuilder(user, pci)
+        ),
+    },
+    authDescriptor: {
+      add: (
+        newUser: User,
+        accountId: BufferId //add user? needs refactoring
+      ) =>
+        addAuthDescriptorToAccount(
+          newUser,
+          accountId,
+          legacyTransactionBuilder(user, pci)
+        ),
+      deleteAllExcluding: (authDescriptorId: BufferId, accountId: BufferId) =>
+        deleteAllAuthDescriptorsExclude(
+          authDescriptorId,
+          accountId,
+          legacyTransactionBuilder(user, pci)
+        ),
+      delete: (authDescriptorId: BufferId, accountId: BufferId) =>
+        deleteAuthDescriptor(
+          authDescriptorId,
+          accountId,
+          legacyTransactionBuilder(user, pci)
+        ),
+    },
+    token: {
+      transfer: (
+        from: BufferId,
+        to: BufferId,
+        asset: BufferId,
+        amount: Amount
+      ) =>
+        transfer(from, to, asset, amount, legacyTransactionBuilder(user, pci)),
+      burn: (from: BufferId, asset: BufferId, amount: Amount) =>
+        burnTokens(asset, amount, legacyTransactionBuilder(user, pci)),
+      xcTransfer: () => xcTransfer(),
+    },
+    admin: {
+      register: (adminUser, authDescriptor: AuthDescriptor) =>
+        registerAccount(user, adminUser, pci, authDescriptor),
+      givePoints: (adminUser, accountId: BufferId, points: number) =>
+        givePoints(user, adminUser, pci, accountId, points),
+    },
+  });
---
+++ client/lib/ft3/accounts/account/payment-history/types.ts
@@ -0,0 +1,75 @@
+import { Amount } from "../../asset/interfaces";
+import { PageCursor } from "../../types";
+
+export type PaymentHistoryTransferArgs = {
+  amount: Amount;
+  accountId: Buffer;
+};
+
+export type PaymentHistoryEntryResponse = {
+  id: string;
+  delta: bigint;
+  decimals: number;
+  asset: string;
+  asset_id: Buffer;
+  is_input: number;
+  timestamp: number;
+  block_height: number;
+  entry_index: number;
+  transfer_args: string;
+  tx_rid: Buffer;
+  tx_data: string;
+};
+
+export type PaymentHistoryEntry = {
+  rowid: string;
+  isInput: boolean;
+  delta: Amount;
+  asset: AssetInfo;
+  entryIndex: number;
+  data: Buffer;
+  transferInputArgs: PaymentHistoryTransferArgs[];
+  transferOutputArgs: PaymentHistoryTransferArgs[];
+  timestamp: Date;
+  transactionId: Buffer;
+  blockHeight: number;
+  //brid: Buffer;
+};
+
+export type TransferHistoryResponse = {
+  data: PaymentHistoryEntry[];
+  nextCursor: PageCursor | null;
+};
+
+export type AssetInfo = {
+  name: string;
+  id: Buffer;
+};
+
+export enum PaymentHistoryType {
+  Sent = 0,
+  Received = 1,
+}
+
+export type PaymentHistoryFilter = {
+  paymentHistoryType?: PaymentHistoryType;
+};
+
+export type PaymentHistoryJSON = {
+  rowid: string;
+  isInput: boolean;
+  delta: string;
+  decimals: number;
+  assetName: string;
+  assetId: string;
+  entryIndex: number;
+  data: string;
+  transferArgs: {
+    amount: number;
+    accountId: string;
+  }[][];
+  timestamp: number;
+  transactionId: string;
+  blockHeight: number;
+  //brid: brid.toString("hex"),
+};
