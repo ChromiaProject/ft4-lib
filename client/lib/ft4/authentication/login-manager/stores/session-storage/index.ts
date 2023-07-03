50a9def89f497322f0cc6e63bbaf0b26102989e4
Joso Marković <joso.markovic@chromaway.com>
2023-07-03 11:06:03 +0200

    FT4-90 - Fix login key store name

--- client/lib/ft4/authentication/login-manager/stores/session-storage/index.ts
+++ client/lib/ft4/authentication/login-manager/stores/session-storage/index.ts
@@ -1,6 +1,6 @@
 import { createBrowserLoginKeyStore } from "../abstract-browser-storage";
 import { LoginKeyStore } from "../types";
 
-export function createSessionStorageKeyStore(): LoginKeyStore {
+export function createSessionStorageLoginKeyStore(): LoginKeyStore {
   return createBrowserLoginKeyStore(sessionStorage);
 }
