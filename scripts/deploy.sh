db805b6fd507ac207cea5d0a6a8a69345b3a7bcf
FrancescoSnt <francesco.santanche@chromaway.com>
2023-07-04 14:26:08 +0100

    started edit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 which="latest"
+OLD_BRID=$CURRENT_BRID_LATEST
+VARIABLE_UUID="98de4c85-1f75-4dc9-85b4-c99fa59bcd76"
 while :; do
     case $1 in
         --stable)
               echo 'Deploying to stable'
               which="stable"
+              OLD_BRID=$CURRENT_BRID_STABLE
+              VARIABLE_UUID="3539621a-e2e1-4f18-9667-2b2fe3b7ca61"
               ;;
         --)
             shift
@@ -23,15 +27,37 @@ done
 echo "\nBuilding..."
 chr build -s configs/devnet1.yaml
 
+
 echo "\nPausing the old network..."
+sed -E -i 's/x"[0-9A-F]{64}" #'$which'/x"'$OLD_BRID'" #'$which'/' configs/devnet1.yaml
 chr deployment pause -d $which -bc ft_deploy -s configs/devnet1.yaml
 sed -E -i 'N;s/chains:\n\s+ft_deploy: x"[0-9A-F]{64}" #'$which'/#'$which'/;P;D' configs/devnet1.yaml
 
 echo -n "\nDeploying a new one... "
 BRID=$( echo y | chr deployment create -d $which -bc ft_deploy -s configs/devnet1.yaml | grep -oE '[0-9A-F]{64}' )
+
+if [ -z "$BRID" ]; then
+    printf "Error during deployment!\n" >&2
+    exit 1
+fi
+
 echo "new brid: $BRID"
 
-sed -E -i 's/#'$which'/chains:\n      ft_deploy: x"'$BRID'" #'$which'/' configs/devnet1.yaml
+case $which in
+        "stable")
+              CURRENT_BRID_STABLE=
+              ;;
+        --)
+            shift
+            break
+            ;;
+        -?*)
+            printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
+            ;;
+        *)
+            break
+            ;;
+    esac
 
 curl -X POST https://chromadev.zulipchat.com/api/v1/messages \
     -u $BOT_EMAIL_ADDRESS:$BOT_API_KEY \
