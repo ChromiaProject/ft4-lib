92f606819d7d3f3fb56498a874d4203ed84135ce
FrancescoSnt <francesco.santanche@chromaway.com>
2023-07-06 18:25:59 +0100

    fixes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 which="latest"
 OLD_BRID=$CURRENT_BRID_LATEST
 VARIABLE_UUID="98de4c85-1f75-4dc9-85b4-c99fa59bcd76"
-WALLET_VARIABLE_UUID="xxx"
+WALLET_VARIABLE_UUID="a5191218-79f9-4b48-99ed-4ac0bf138bea"
 while :; do
     case $1 in
         --stable)
@@ -10,6 +10,7 @@ while :; do
               which="stable"
               OLD_BRID=$CURRENT_BRID_STABLE
               VARIABLE_UUID="3539621a-e2e1-4f18-9667-2b2fe3b7ca61"
+              WALLET_VARIABLE_UUID="4cb2f3f6-3f52-4b66-b37c-532825307b1b"
               ;;
         --)
             shift
@@ -25,12 +26,23 @@ while :; do
     shift
 done
 
+ZULIP_MESSAGE="Releasing: ${which}.\n"
+EXIT_EARLY=0
+
 echo "\nBuilding..."
+
+if [ $which = "stable" ]; then
+    printf "// stable" >> rell/src/main/module.rell # makes unique brid
+fi
 chr build -s configs/devnet1.yaml
 
 echo "\nPausing the old network..."
 sed -E -i 's/x"[0-9A-F]{64}" #'$which'/x"'$OLD_BRID'" #'$which'/' configs/devnet1.yaml
 chr deployment pause -d $which -bc ft_deploy -s configs/devnet1.yaml
+
+PAUSED_OLD_CHAIN=$?
+txt_to_add=$( [ $PAUSED_OLD_CHAIN -eq 0 ] && echo "Old chain paused" || echo "Couldn't pause old chain")
+
 sed -E -i 'N;s/chains:\n\s+ft_deploy: x"[0-9A-F]{64}" #'$which'/#'$which'/;P;D' configs/devnet1.yaml
 
 echo -n "\nDeploying a new one... "
@@ -38,10 +50,26 @@ BRID=$( echo y | chr deployment create -d $which -bc ft_deploy -s configs/devnet
 
 if [ -z "$BRID" ]; then
     printf "Error during deployment!\n" >&2
+    ZULIP_MESSAGE="${ZULIP_MESSAGE}Failed! ${txt_to_add}"
+    EXIT_EARLY=1
+else 
+    ZULIP_MESSAGE="${ZULIP_MESSAGE}Success! ${txt_to_add}\n\nBRID:`$BRID`"
+fi
+
+curl -X POST https://chromadev.zulipchat.com/api/v1/messages \
+    -u $BOT_EMAIL_ADDRESS:$BOT_API_KEY \
+    --data-urlencode type=stream \
+    --data-urlencode 'to="Chromia Wallet"' \
+    --data-urlencode 'topic=deploy bot' \
+    --data-urlencode "content=$ZULIP_MESSAGE" 
+
+if [ "$EXIT_EARLY" -eq 0 ]; then
     exit 1
 fi
 
-echo "new brid: $BRID"
+printf "\nDone!\nNew brid: $BRID\n"
+
+
 echo "editing pipeline variable on ft3-lib repo... "
 curl --request PUT \
   --url 'https://api.bitbucket.org/2.0/repositories/chromawallet/ft3-lib/pipelines_config/variables/%7B'$VARIABLE_UUID'%7D' \
@@ -57,12 +85,3 @@ curl --request PUT \
 #         --data '{"value":"'$BRID'"}' \
 #         --header "Authorization: Bearer $WALLET_BRID_UPDATER"
 # fi
-
-printf "\nDone!\n"
-
-curl -X POST https://chromadev.zulipchat.com/api/v1/messages \
-    -u $BOT_EMAIL_ADDRESS:$BOT_API_KEY \
-    --data-urlencode type=stream \
-    --data-urlencode 'to="Chromia Wallet"' \
-    --data-urlencode 'topic=deploy bot' \
-    --data-urlencode "content=New $which release! BRID: \`$BRID\`"
