751fbb71381b322d0d2100fa2ff355c723f0d466
Joso Marković <joso.markovic@chromaway.com>
2023-07-06 08:53:52 +0200

    FT4-178 - Mark old queries as deprecated

--- client/lib/ft4/asset/asset-queries.ts
+++ client/lib/ft4/asset/asset-queries.ts
@@ -4,14 +4,14 @@ import { Query } from "../utils/types";
 import { OptionalPageCursor } from "../types";
 import { Buffer } from "buffer";
 
-/*
+/**
  * @deprecated
  */
 export function assetByIdQuery(assetId: Buffer): Query {
   return ["ft4.get_asset_by_id", { asset_id: assetId }];
 }
 
-/*
+/**
  * @deprecated
  */
 export function balancesByAccountIdQuery(accountId: BufferId): Query {
@@ -21,7 +21,7 @@ export function balancesByAccountIdQuery(accountId: BufferId): Query {
   ];
 }
 
-/*
+/**
  * @deprecated
  */
 export function balanceQuery(accountId: Buffer, assetId: Buffer): Query {
@@ -34,14 +34,14 @@ export function balanceQuery(accountId: Buffer, assetId: Buffer): Query {
   ];
 }
 
-/*
+/**
  * @deprecated
  */
 export function assetByNameQuery(name: string): Query {
   return ["ft4.get_asset_by_name", { name: name }];
 }
 
-/*
+/**
  * @deprecated
  */
 export function allAssetsQuery(): Query {
