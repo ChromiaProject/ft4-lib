fd88d3c17f483174714969d912a24dcc0ca8ad2f
Konstantin Alexandrov <konstantin.alexandrov@kindly.ai>
2023-07-07 12:26:15 +0200

    Update demo app test

--- examples/demo/client/src/App.test.js
+++ examples/demo/client/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/FT4 Demo App/i);
+  expect(headerElement).toBeInTheDocument();
 });
