a88f822ff96943c5f71c3dc2b620aee3139d683d
Konstantin Alexandrov <konstantin.alexandrov@kindly.ai>
2023-07-07 11:52:32 +0200

    Fix React App layout

--- examples/demo/client/src/App.js
+++ examples/demo/client/src/App.js
@@ -3,9 +3,11 @@ import { createClient } from 'postchain-client';
 import { createAmount, createKeyStoreInteractor, createWeb3ProviderEvmKeyStore } from 'ft3-lib';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 function App() {
-  const [session, setSession] = useState(null); // define session state
+  const [session, setSession] = useState(null);
   const [accounts, setAccounts] = useState([]);
   const [assets, setAssets] = useState([]);
   const [receiverId, setReceiverId] = useState('');
@@ -14,62 +16,106 @@ function App() {
     const url = 'http://localhost:7740';
     const rid = '22F97053D106E8A2D6E2C633347CC3A0D4171003DDB3D69E53DEF79D0B9630C7';
 
-    createClient({
+    const initializeSession = async () => {
+      const client = await createClient({
         nodeURLPool: url,
         blockchainRID: rid,
-    })
-    .then((client) => {
-      createWeb3ProviderEvmKeyStore(window.ethereum).then(async store => {
-        const { getAccounts, getLoginManager } = createKeyStoreInteractor(client, store); 
-  
-        const accountsData = await getAccounts();
-        setAccounts(accountsData);
-  
-        if (!accountsData.length) {
-            console.log("No accounts found");
-            return;
-        }
+      });
 
-        const newSession = await getLoginManager().login({
-            accountId: accountsData[0].id,
-        })
-        setSession(newSession);
+      const store = await createWeb3ProviderEvmKeyStore(window.ethereum);
+      const { getAccounts, getLoginManager } = createKeyStoreInteractor(client, store);
 
+      const accountsData = await getAccounts();
+      setAccounts(accountsData);
+
+      if (!accountsData.length) {
+        console.log("No accounts found");
+        return;
+      }
+
+      const newSession = await getLoginManager().login({
+        accountId: accountsData[0].id,
+      });
+
+      setSession(newSession);
+    };
+
+    initializeSession();
+  }, []);
+
+  useEffect(() => {
+    const getAssets = async () => {
+      if (session) {
         const assetsData = await session.getAllAssets();
         setAssets(assetsData);
 
         if (!assetsData.length) {
           console.log("No assets found");
         }
-      });
-    })
-  });
+      }
+    };
+
+    getAssets();
+  }, [session]);
 
   const handleTransfer = async () => {
     await session.account.transfer(receiverId, assets[0].id, createAmount(12, 6));
   };
 
-  return (
-    <div className="App">
-      <div>
-        {accounts.length ? accounts.map((account, index) => (
-          <p key={index}>Account: {account.id}</p>
-        )) : <p>No accounts found</p>}
-      </div>
-      <div>
-        {assets.length ? assets.map((asset, index) => (
-          <div key={index}>
-            <img src={asset.icon_url} alt={asset.name} />
-            <p>{asset.name}</p>
-          </div>
-        )) : <p>No assets found</p>}
-      </div>
-      <div>
-        <TextField label="Receiver ID" variant="outlined" onChange={e => setReceiverId(e.target.value)} />
-        <Button variant="contained" onClick={handleTransfer}>Transfer</Button>
-      </div>
-    </div>
-  );
+return (
+  <Box
+    className="App"
+    display="flex"
+    flexDirection="column"
+    alignItems="center"
+    justifyContent="center"
+    style={{ minHeight: '100vh', textAlign: 'center' }}
+  >
+    <Typography variant="h3" gutterBottom>
+      FT4 Demo App
+    </Typography>
+    {accounts.length ? (
+      accounts.map((account, index) => (
+        <Typography key={index} variant="h5" component="div" gutterBottom>
+          <div>Account</div>
+          <div><strong>{account.id}</strong></div>
+        </Typography>
+      ))
+    ) : (
+      <Typography>No accounts found</Typography>
+    )}
+    {assets.length ? (
+      assets.map((asset, index) => (
+        <Typography key={index} variant="h5" component="div" gutterBottom style={{ marginTop: '1rem' }}>
+          <div>Asset</div>
+          <div><strong>{asset.name}</strong></div>
+          <img src={asset.icon_url} alt={asset.name} style={{ height: '50px' }} />
+        </Typography>
+      ))
+    ) : (
+      <Typography>No assets found</Typography>
+    )}
+    <Box sx={{ margin: '1rem 0' }}>
+      <TextField
+        label="Receiver ID"
+        variant="outlined"
+        onChange={e => setReceiverId(e.target.value)}
+        sx={{ marginBottom: '1rem', width: '300px' }}
+      />
+    </Box>
+    <Box>
+      <Button
+        variant="contained"
+        onClick={handleTransfer}
+        disabled={!receiverId}
+        sx={{ fontSize: '1.2rem', padding: '0.8rem 1.6rem' }}
+      >
+        Transfer
+      </Button>
+    </Box>
+  </Box>
+);
+
 }
 
 export default App;
