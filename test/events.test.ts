6e402a533591f1dc02c60808b222e5a9d3f6892f
Konstantin Alexandrov <konstantin.alexandrov@kindly.ai>
2023-07-25 18:18:55 +0200

    Update tests for the new emitter module

--- test/events.test.ts
+++ test/events.test.ts
@@ -1,13 +1,47 @@
-import { ftEventEmitter } from "../client/lib/ft4/events";
+import {
+  Listener,
+  onAccountAddressChange,
+  offAccountAddressChange,
+} from "../client/lib/ft4/events";
+import { ftEventEmitter } from "../client/lib/ft4/events/emitter";
 
-describe("FTEventEmitter", () => {
-  test("should emit and handle addressChange event correctly", () => {
-    const mockFn = jest.fn();
-    ftEventEmitter.on("addressChange", mockFn);
+describe("EventEmitter functionality", () => {
+  let listener: Listener<[string]>;
+  const newAddress = "newAddressTest";
 
-    const testAddress = "0x123456789abcdef";
-    ftEventEmitter.emit("addressChange", testAddress);
+  beforeEach(() => {
+    listener = jest.fn(); // Mock function
+  });
 
-    expect(mockFn).toBeCalledWith(testAddress);
+  afterEach(() => {
+    ftEventEmitter.off("AccountAddressChange", listener); // Clean up after each test
+  });
+
+  it("should register an event listener", () => {
+    onAccountAddressChange(listener);
+    expect(ftEventEmitter["events"]["AccountAddressChange"]).toContain(
+      listener,
+    );
+  });
+
+  it("should call the event listener when the event is emitted", () => {
+    onAccountAddressChange(listener);
+    ftEventEmitter.emit("AccountAddressChange", newAddress);
+    expect(listener).toHaveBeenCalledWith(newAddress);
+  });
+
+  it("should remove the event listener", () => {
+    onAccountAddressChange(listener);
+    offAccountAddressChange(listener);
+    expect(ftEventEmitter["events"]["AccountAddressChange"]).not.toContain(
+      listener,
+    );
+  });
+
+  it("should not call the event listener after it's been removed", () => {
+    onAccountAddressChange(listener);
+    offAccountAddressChange(listener);
+    ftEventEmitter.emit("AccountAddressChange", newAddress);
+    expect(listener).not.toHaveBeenCalled();
   });
 });
