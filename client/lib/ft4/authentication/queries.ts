be6ec528569c198cc49b66fef2c4792bb6ddbba2
Linus Lagerhjelm <linus.lagerhjelm@omegapoint.se>
2023-08-14 11:57:15 +0200

    Rename 'strict' to 'is_strict'

--- client/lib/ft4/authentication/queries.ts
+++ client/lib/ft4/authentication/queries.ts
@@ -27,25 +27,25 @@ export function loginConfig(
 
 export function authFlags(
   operation: Operation
-): QueryObject<{ op_name: string; strict: boolean }> {
+): QueryObject<{ op_name: string; is_strict: boolean }> {
   return {
     name: "ft4.get_auth_flags",
     args: {
       op_name: operation.name,
-      strict: true,
+      is_strict: true,
     },
   };
 }
 
 export function authMessageTemplate(
   operation: Operation
-): QueryObject<{ op_name: string; op_args?: RawGtv[]; strict: boolean }> {
+): QueryObject<{ op_name: string; op_args?: RawGtv[]; is_strict: boolean }> {
   return {
     name: "ft4.get_auth_message_template",
     args: {
       op_name: operation.name,
       op_args: operation.args,
-      strict: true,
+      is_strict: true,
     },
   };
 }
