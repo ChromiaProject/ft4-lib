5738893b35a64b54e2e1ab3937c864c9ad7d4ba6
FrancescoSnt <francesco.santanche@chromaway.com>
2023-09-28 08:25:30 +0100

    fixed camelcase

--- client/lib/ft4/crosschain/orchestrator.ts
+++ client/lib/ft4/crosschain/orchestrator.ts
@@ -20,7 +20,7 @@ import { transactionBuilder } from "../utils/transaction-builder";
 import { createNoopAuthenticator } from "../authentication";
 import { createAuthDataService } from "../ft-session";
 import { Orchestrator, OrchestratorEvents } from "./types";
-import { getTransactionRID } from "../utils";
+import { getTransactionRid } from "../utils";
 
 type State = {
   currentHopIndex: number;
@@ -136,12 +136,12 @@ export async function createOrchestrator(
 
     const sourceBlockchainRid =
       pathIndex === 0
-        ? session.client.config.blockchainRID
+        ? session.client.config.blockchainRid
         : path[pathIndex - 1];
 
     const proofTx = createIccfProofTx(
       directoryClient,
-      getTransactionRID(state.tx),
+      getTransactionRid(state.tx),
       gtv.gtvHash(decodedTx),
       decodedTx.signers,
       sourceBlockchainRid.toString("hex"),
@@ -158,8 +158,8 @@ export async function createOrchestrator(
    */
   async function transfer(): Promise<void> {
     const directoryClient = await createClient({
-      directoryNodeURLPool: session.client.config.endpointPool.slice(),
-      blockchainIID: 0,
+      directoryNodeUrlPool: session.client.config.endpointPool.slice(),
+      blockchainIid: 0,
     });
 
     try {
