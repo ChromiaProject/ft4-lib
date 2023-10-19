6d6bfb70f972fa8d4e9f4eb88d08eb7ad7490720
Joso Marković <joso.markovic@chromaway.com>
2023-10-19 16:08:55 +0200

    0.1.5r

--- test/blockchain.test.ts
+++ test/blockchain.test.ts
@@ -27,7 +27,7 @@ describe("Blockchain", () => {
   it("should provide ft4 rell-side version number", async () => {
     const info = await connection.getVersion();
 
-    expect(info).toEqual("0.1.4");
+    expect(info).toEqual("0.1.5");
 
     expect(ft.getClientVersion()).toEqual(version);
   });
