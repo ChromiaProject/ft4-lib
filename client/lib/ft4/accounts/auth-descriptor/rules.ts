7ecb18f6fba9b77b258790c155766e9037c620bd
Linus Lagerhjelm <linus.lagerhjelm@omegapoint.se>
2023-11-03 08:23:48 +0100

    It is now possible to pass a single rule.

--- client/lib/ft4/accounts/auth-descriptor/rules.ts
+++ client/lib/ft4/accounts/auth-descriptor/rules.ts
@@ -23,50 +23,60 @@ function chooseOperator(start: any[], variable: number) {
   return {
     lessThan: (value: number) => {
       const current = start
-        ? start.concat([[variable, RuleOperator.LessThan, value]])
-        : [[variable, RuleOperator.LessThan, value]];
+        ? start[0] === "and"
+          ? [...start, [variable, RuleOperator.LessThan, value]]
+          : ["and", start, [variable, RuleOperator.LessThan, value]]
+        : [variable, RuleOperator.LessThan, value];
       return {
-        only: <AuthDescriptorRule>Object.freeze(["and", ...current]),
+        only: <AuthDescriptorRule>Object.freeze(current),
         and: chooseVariable(current),
       };
     },
 
     lessOrEqual: (value: number) => {
       const current = start
-        ? start.concat([[variable, RuleOperator.LessOrEqual, value]])
-        : [[variable, RuleOperator.LessOrEqual, value]];
+        ? start[0] === "and"
+          ? [...start, [variable, RuleOperator.LessOrEqual, value]]
+          : ["and", start, [variable, RuleOperator.LessOrEqual, value]]
+        : [variable, RuleOperator.LessOrEqual, value];
       return {
-        only: <AuthDescriptorRule>Object.freeze(["and", ...current]),
+        only: <AuthDescriptorRule>Object.freeze(current),
         and: chooseVariable(current),
       };
     },
 
     equals: (value: number) => {
       const current = start
-        ? start.concat([[variable, RuleOperator.Equals, value]])
-        : [[variable, RuleOperator.Equals, value]];
+        ? start[0] === "and"
+          ? [...start, [variable, RuleOperator.Equals, value]]
+          : ["and", start, [variable, RuleOperator.Equals, value]]
+        : [variable, RuleOperator.Equals, value];
       return {
-        only: <AuthDescriptorRule>Object.freeze(["and", ...current]),
+        only: <AuthDescriptorRule>Object.freeze(current),
         and: chooseVariable(current),
       };
     },
 
     greaterOrEqual: (value: number) => {
       const current = start
-        ? start.concat([[variable, RuleOperator.GreaterOrEqual, value]])
-        : [[variable, RuleOperator.GreaterOrEqual, value]];
+        ? start[0] === "and"
+          ? [...start, [variable, RuleOperator.GreaterOrEqual, value]]
+          : ["and", start, [variable, RuleOperator.GreaterOrEqual, value]]
+        : [variable, RuleOperator.GreaterOrEqual, value];
       return {
-        only: <AuthDescriptorRule>Object.freeze(["and", ...current]),
+        only: <AuthDescriptorRule>Object.freeze(current),
         and: chooseVariable(current),
       };
     },
 
     greaterThan: (value: number) => {
       const current = start
-        ? start.concat([[variable, RuleOperator.GreaterThen, value]])
-        : [[variable, RuleOperator.GreaterThen, value]];
+        ? start[0] === "and"
+          ? [...start, [variable, RuleOperator.GreaterThen, value]]
+          : ["and", start, [variable, RuleOperator.GreaterThen, value]]
+        : [variable, RuleOperator.GreaterThen, value];
       return {
-        only: <AuthDescriptorRule>Object.freeze(["and", ...current]),
+        only: <AuthDescriptorRule>Object.freeze(current),
         and: chooseVariable(current),
       };
     },
