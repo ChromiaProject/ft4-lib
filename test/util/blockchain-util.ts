49739d84f227f84a176cbe5ae01858eef290be83
Mikael Ståldal <mikael.staldal@chromaway.com>
2023-11-29 11:24:48 +0100

    Fix mock client

--- test/util/blockchain-util.ts
+++ test/util/blockchain-util.ts
@@ -35,7 +35,8 @@ export async function createChromiaClient(nodeUrl?: string, iid = 0) {
 export async function createMockClient() {
   return createClient({
     nodeUrlPool: "http://127.0.0.1:7740",
-    blockchainRid: "-",
+    blockchainRid:
+      "0000000000000000000000000000000000000000000000000000000000000000",
   });
 }
 
