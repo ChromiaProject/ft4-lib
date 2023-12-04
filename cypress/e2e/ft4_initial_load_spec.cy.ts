28dd8dff77fa80e6a6a7a7f7f5c7f653f262d55c
Konstantin Alexandrov <konstantin.alexandrov@kindly.ai>
2023-12-04 17:13:27 +0100

    Changelog updated, unnecessary comments removed from spec

--- cypress/e2e/ft4_initial_load_spec.cy.ts
+++ cypress/e2e/ft4_initial_load_spec.cy.ts
@@ -1,7 +1,6 @@
 describe('FT4 Library Browser Test', () => {
   it('loads the FT4 library without crashing', () => {
-    cy.visit('/'); // Adjust this if your app's URL is different
-    // Add more actions or assertions as needed
-    cy.contains('FT4 Demo App'); // Adjust based on the actual content of your app
+    cy.visit('/');
+    cy.contains('FT4 Demo App');
   });
 });
