8df3a19aa29d1843584fcf21868f0fbf2d608db8
Linus Lagerhjelm <linus.lagerhjelm@omegapoint.se>
2023-12-11 13:18:47 +0100

    WIP2

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,13 @@
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import inject from "@rollup/plugin-inject";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import nodeResolve from "@rollup/plugin-node-resolve";
 import alias from "@rollup/plugin-alias";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { cwd } from "process";
+import path from "path";
+
+const applicationRoot = path.resolve(cwd(), 'client', 'lib', 'ft4')
 
 export default [
   //ESM
@@ -14,54 +19,61 @@ export default [
       name: "FT4 lib",
       sourcemap: true,
     },
-    external: ['postchain-client'],
     plugins: [
-      nodeResolve(),
-      commonjs({transformMixedEsModules: true}),
-      inject({ Buffer: ["buffer", "Buffer"] }),
-      json(),
       alias({
         entries: [
-          { find: "crypto", replacement: "crypto-browserify" },
-          { find: "stream", replacement: "stream-browserify" },
-        ],
+          { find: /^\/ft4\/(.*)/, replacement: 'client/lib/ft4/$1' }
+        ]
       }),
+      nodeResolve(),
+      commonjs({transformMixedEsModules: true}),
+      peerDepsExternal(),
+      inject({ Buffer: ["buffer", "Buffer"] }),
+      json(),
+      // alias({
+      //   // applicationRoot: `${cwd()}/client/lib`,
+      //   entries: [
+      //     { find: "crypto", replacement: "crypto-browserify" },
+      //     { find: "stream", replacement: "stream-browserify" },
+      //   ],
+      // }),
     ],
   },
   //UMD
-  {
-    input: "./dist/index.js",
-    output: {
-      dir: "./dist/umd",
-      format: "umd",
-      name: "FT4 lib",
-      sourcemap: true,
-    },
-    external: ['postchain-client'],
-    plugins: [
-      commonjs(),
-      inject({ Buffer: ["buffer", "Buffer"] }),
-      json(),
-      nodeResolve(),
-      alias({
-        entries: [
-          { find: "crypto", replacement: "crypto-browserify" },
-          { find: "stream", replacement: "stream-browserify" },
-        ],
-      }),
-    ],
-  },  //NODE
-  {
-    input: "./dist/index.js",
-    output: {
-      dir: "./dist/cjs",
-      format: "cjs",
-      name: "FT4 lib",
-      sourcemap: true,
-    },
-    plugins: [
-      commonjs(),
-      json()
-    ],
-  },
+  // {
+  //   input: "./dist/index.js",
+  //   output: {
+  //     dir: "./dist/umd",
+  //     format: "umd",
+  //     name: "FT4 lib",
+  //     sourcemap: true,
+  //   },
+  //   plugins: [
+  //     // nodeResolve({ preferBuiltins: false }),
+  //     commonjs({ transformMixedEsModules: true }),
+  //     peerDepsExternal(),
+  //     inject({ Buffer: ["buffer", "Buffer"] }),
+  //     json(),
+  //     alias({
+  //       // applicationRoot: `${cwd()}/client/lib`,
+  //       entries: [
+  //         { find: "crypto", replacement: "crypto-browserify" },
+  //         { find: "stream", replacement: "stream-browserify" },
+  //       ],
+  //     }),
+  //   ],
+  // },  //NODE
+  // {
+  //   input: "./dist/index.js",
+  //   output: {
+  //     dir: "./dist/cjs",
+  //     format: "cjs",
+  //     name: "FT4 lib",
+  //     sourcemap: true,
+  //   },
+  //   plugins: [
+  //     commonjs(),
+  //     json()
+  //   ],
+  // },
 ];
