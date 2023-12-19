bbb8748d03a48b0a881b1faa8c3c600ac8a64219
Konstantin Alexandrov <konstantin.alexandrov@kindly.ai>
2023-12-19 10:41:58 +0100

    Fix E2E tests logging

--- scripts/start-e2e-environment.sh
+++ scripts/start-e2e-environment.sh
@@ -4,6 +4,7 @@
 DEMO_PATH="examples/demo"
 RELL_PATH="$DEMO_PATH/rell"
 FRONTEND_PATH="$DEMO_PATH/client"
+LOG_PATH="$(pwd)/logs"
 
 # Keypair configurations
 KEYPAIR=".admin_keypair"
@@ -68,8 +69,7 @@ start_backend() {
 
     # Start Node
     echo "Starting Chromia node..."
-    mkdir -p ./logs
-    chr node start --wipe > ./logs/e2e-postchain.log 2>&1 &
+    chr node start --wipe > "$LOG_PATH/e2e-postchain.log" 2>&1 &
     NODE_PID=$!
 
     cd - > /dev/null
@@ -120,15 +120,15 @@ setup_blockchain_resources() {
 
     echo "Registering account with Ethereum address..."
     REGISTER_ACCOUNT_RESULT=$(chr tx ft4.admin.register_account \
-        "[0, [['A','T'], x'${ETH_ADDRESS:2}'], null]" \
+        "[0, [['A','T'], x\"${ETH_ADDRESS:2}\"], null]" \
         --cid 0 --await --secret $KEYPAIR_PATH)
     echo "Account registration result: $REGISTER_ACCOUNT_RESULT"
 
-    # echo "Registering test asset..."
-    # REGISTER_ASSET_RESULT=$(chr tx ft4.admin.register_asset \
-    #     TestAsset TST 6 http://url-to-asset-icon \
-    #     --cid 0 --await --secret $KEYPAIR_PATH)
-    # echo "Asset Registration Result: $REGISTER_ASSET_RESULT"
+    echo "Registering test asset..."
+    REGISTER_ASSET_RESULT=$(chr tx ft4.admin.register_asset \
+        TestAsset TST 6 http://url-to-asset-icon \
+        --cid 0 --await --secret $KEYPAIR_PATH)
+    echo "Asset Registration Result: $REGISTER_ASSET_RESULT"
 
     # echo "Minting asset to account..."
     # MINT_ASSET_RESULT=$(chr tx ft4.admin.mint \
@@ -219,6 +219,8 @@ for arg in "$@"; do
             ;;
     esac
 done
+
+mkdir -p "$LOG_PATH"
 
 # Check command to run is specified
 if [ -z "$COMMAND_TO_RUN" ]; then
