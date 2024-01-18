670d389f5c7013dc04965b5a7dc6477da08ec601
Mikael Ståldal <mikael.staldal@chromaway.com>
2024-01-18 13:23:09 +0100

    Make install script more silent

--- install-chr-and-pmc.sh
+++ install-chr-and-pmc.sh
@@ -2,9 +2,9 @@
 
 # install chromia-cli-0.15.1-dist.tar.gz
 mkdir -p /opt/chromaway/chr
-wget -O - https://gitlab.com/chromaway/core-tools/chromia-cli/-/package_files/109535964/download | tar -C /opt/chromaway/chr -xz
+wget -nv -O - https://gitlab.com/chromaway/core-tools/chromia-cli/-/package_files/109535964/download | tar -C /opt/chromaway/chr -xz
 ln -s /opt/chromaway/chr/bin/chr /bin/chr
 
 # install management-console-3.19.2-dist.tar.gz
-wget -O - https://gitlab.com/chromaway/core-tools/management-console/-/package_files/109244030/download | tar -C /opt/chromaway -xz
+wget -nv -O - https://gitlab.com/chromaway/core-tools/management-console/-/package_files/109244030/download | tar -C /opt/chromaway -xz
 ln -s /opt/chromaway/management-console/bin/pmc /bin/pmc
