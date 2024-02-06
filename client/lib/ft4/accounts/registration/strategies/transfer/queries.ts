ec1042fdffa3511d584c797f3ed9b0c3279d4a02
Mikael Ståldal <mikael.staldal@chromaway.com>
2024-02-06 18:12:26 +0300

    Return set of strings from get_pending_transfer_strategies

--- client/lib/ft4/accounts/registration/strategies/transfer/queries.ts
+++ client/lib/ft4/accounts/registration/strategies/transfer/queries.ts
@@ -2,7 +2,7 @@ import { QueryObject } from "postchain-client";
 
 export function getPendingTransferStrategies(
   recipientId: Buffer,
-): QueryObject<string, { recipient_id: Buffer }> {
+): QueryObject<string[] | null, { recipient_id: Buffer }> {
   return {
     name: "ft4.get_pending_transfer_strategies",
     args: {
