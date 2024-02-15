580799491b79feacac919e5e032ede83104f240c
Joso Marković <joso.markovic@chromaway.com>
2024-02-15 15:29:44 +0100

    Export account registration symbols

--- client/lib/ft4/index.ts
+++ client/lib/ft4/index.ts
@@ -120,6 +120,26 @@ export {
   initTransfer,
 } from "./crosschain";
 
+import * as registration from "@ft4/accounts/registration";
+import * as openStrategy from "@ft4/accounts/registration/strategies/open";
+import * as transferStrategy from "@ft4/accounts/registration/strategies/transfer";
+import * as transferOpenStrategy from "@ft4/accounts/registration/strategies/transfer/open";
+import * as transferFeeStrategy from "@ft4/accounts/registration/strategies/transfer/fee";
+
+export const accounts = Object.freeze({
+  registerAccount: registration.registerAccount,
+  strategy: Object.freeze({
+    open: openStrategy.open,
+    transferOpen: transferOpenStrategy.transferOpen,
+    transferFee: transferFeeStrategy.transferFee,
+  }),
+  query: Object.freeze({
+    allowedAssets: transferStrategy.allowedAssets,
+    pendingTransferStrategies: transferStrategy.pendingTransferStrategies,
+    feeAssets: transferFeeStrategy.feeAssets,
+  }),
+});
+
 export const ft = Object.freeze({
   setLogLevel: logger.setLogLevel,
 });
