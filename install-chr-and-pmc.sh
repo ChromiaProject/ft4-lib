e63cf6ae34acd19c87c4598d0da13c1233aa0a4e
Mikael Ståldal <mikael.staldal@chromaway.com>
2024-04-26 08:12:25 +0000

    Upgrade versions of Chromia CLI and PMC
    Approved-by: Joso Markovic

--- install-chr-and-pmc.sh
+++ install-chr-and-pmc.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# install chromia-cli-0.17.2-dist.tar.gz
+# install chromia-cli-0.17.4-dist.tar.gz
 mkdir -p /opt/chromaway/chr
-wget -nv -O - https://gitlab.com/chromaway/core-tools/chromia-cli/-/package_files/122213957/download | tar -C /opt/chromaway/chr -xz
+wget -nv -O - https://gitlab.com/chromaway/core-tools/chromia-cli/-/package_files/123081657/download | tar -C /opt/chromaway/chr -xz
 ln -s /opt/chromaway/chr/bin/chr /bin/chr
 
-# install management-console-3.21.4-dist.tar.gz
-wget -nv -O - https://gitlab.com/chromaway/core-tools/management-console/-/package_files/120749882/download | tar -C /opt/chromaway -xz
+# install management-console-3.21.7-dist.tar.gz
+wget -nv -O - https://gitlab.com/chromaway/core-tools/management-console/-/package_files/124654080/download | tar -C /opt/chromaway -xz
 ln -s /opt/chromaway/management-console/bin/pmc /bin/pmc
