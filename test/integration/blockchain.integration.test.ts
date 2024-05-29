62c4ea8b4b43ad20f70544f7d193d966905dc3df
Joso Markovic <joso.markovic@chromaway.com>
2024-05-29 10:03:49 +0000

    Merged in update-release-notes-0.8.0 (pull request #643)
    Update release notes 0.8.0
    
    * Update release notes
    
    * Update get_version query

--- test/integration/blockchain.integration.test.ts
+++ test/integration/blockchain.integration.test.ts
@@ -32,6 +32,6 @@ describe("Blockchain", () => {
   it("should provide ft4 rell-side version number", async () => {
     const info = await connection.getVersion();
 
-    expect(info).toEqual("0.7.0");
+    expect(info).toEqual("0.8.0");
   });
 });
