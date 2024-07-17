36cd13e3d3375d56075b906fa01dab67c4d7faa4
Vedran Krecl <vedran.krecl@chromaway.com>
2024-07-17 15:57:38 +0200

    amend

--- scripts/multichain-runner.sh
+++ scripts/multichain-runner.sh
@@ -245,8 +245,8 @@ run_main_logic() {
     while [ -z "$BRID" ] && [ $retry_count -lt 500 ]; do
       # Attempt to fetch the value
     #   BRID=$(curl -s http://172.19.0.3:7740/brid/iid_0)
-      BRID=$(curl http://172.19.0.3:7740/brid/iid_0)
-      TEMP=$(curl http://ft4-multichain-test-node:7740/brid/iid_0)
+      BRID=$(curl -s http://172.19.0.3:7740/brid/iid_0)
+      TEMP=$(curl -s http://ft4-multichain-test-node:7740/brid/iid_0)
       debug $BRID
       echo "TEMP!!!!!!!!!!!!!!!"
       debug $TEMP
