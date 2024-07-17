8f71c7b93edf48b1f4ff99793c34e468dcb368ef
Vedran Krecl <vedran.krecl@chromaway.com>
2024-07-17 14:45:48 +0200

    amend

--- scripts/multichain-runner.sh
+++ scripts/multichain-runner.sh
@@ -245,8 +245,11 @@ run_main_logic() {
     while [ -z "$BRID" ] && [ $retry_count -lt 500 ]; do
       # Attempt to fetch the value
     #   BRID=$(curl -s http://172.19.0.3:7740/brid/iid_0)
-      BRID=$(curl -s http://172.19.0.3:7740/brid/iid_0)
+      BRID=$(curl http://172.19.0.3:7740/brid/iid_0)
+      TEMP=$(curl http://ft4-multichain-test-node:7740/brid/iid_0)
       debug $BRID
+      echo "TEMP!!!!!!!!!!!!!!!"
+      debug $TEMP
     #   if [ $retry_count -gt 300 ]; then
         docker ps  
         docker inspect $DOCKER_NODE_NAME
