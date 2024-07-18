07b0557ab994a59559800bba64a626b02c07ed46
Vedran Krecl <vedran.krecl@chromaway.com>
2024-07-18 17:22:48 +0200

    amend

--- scripts/multichain-runner.sh
+++ scripts/multichain-runner.sh
@@ -233,20 +233,20 @@ run_main_logic() {
         -e POSTCHAIN_CONFIG=/config/config.0.properties \
         -e POSTCHAIN_BLOCKCHAIN_CONFIG=/build/manager.xml \
         -p $NODE_PORT:9870/tcp \
-        -p 127.0.0.1:$API_PORT:7740/tcp \
+        -p $API_PORT:7740/tcp \
         registry.gitlab.com/chromaway/postchain-chromia/chromaway/chromia-server:${CHROMIA_NODE_VERSION} \
         run-node > ./multichain-postchain.log &
 
     debug "Fetching manager chain BRID..."
-    BRID=""
+    BRID="111111111"
     retry_count=0
 
     # Loop until BRID receives a non-empty value or until 10 tries
     while [ -z "$BRID" ] && [ $retry_count -lt 500 ]; do
       # Attempt to fetch the value
     #   BRID=$(curl -s http://172.19.0.3:7740/brid/iid_0)
-    #   BRID=$(curl -s http://172.19.0.3:7740/brid/iid_0)
-      BRID=$(curl -s http://127.0.0.1:7740/brid/iid_0)
+      BRID=$(curl -s http://172.19.0.3:7740/brid/iid_0)
+    #   BRID=$(curl -s http://127.0.0.1:7740/brid/iid_0)
       debug $BRID
     #   echo "TEMP!!!!!!!!!!!!!!!"
     #   debug $TEMP
