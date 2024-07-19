cee80b4506f6446e19bb46f3fc2afc68568d4199
Vedran Krecl <vedran.krecl@chromaway.com>
2024-07-19 09:49:17 +0200

    amend

--- scripts/multichain-runner.sh
+++ scripts/multichain-runner.sh
@@ -149,13 +149,13 @@ run_main_logic() {
         exit 1
     fi
 
-    docker network create -d bridge test_network
+    # docker network create -d bridge test_network
+    # --network test_network \
 
     if $postgres; then
       log "Running Postgres container..."
       $DOCKER run \
-          --name $DOCKER_POSTGRES_NAME \
-          --network test_network \
+          --name $DOCKER_POSTGRES_NAME \          
           -e POSTGRES_PASSWORD=postchain \
           -e POSTGRES_USER=postchain \
           -p $POSTGRES_PORT:5432 \
@@ -224,7 +224,6 @@ run_main_logic() {
     # $DOCKER -H $DOCKER_HOST network create -d bridge localnet
     $DOCKER run --privileged \
         --name $DOCKER_NODE_NAME \
-        --network test_network \
         --restart unless-stopped \
         -v "$(pwd)/$BASE_CONFIG_DIR:/config" \
         -v "$(pwd)/$DEPENDENCIES_PATH/directory-chain/build:/build" \
