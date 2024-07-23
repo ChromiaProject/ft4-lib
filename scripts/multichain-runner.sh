d37e34182775a0f20ac79eae87ad15c4984544ee
Vedran Krecl <vedran.krecl@chromaway.com>
2024-07-23 10:50:46 +0200

    amend dockerhost

--- scripts/multichain-runner.sh
+++ scripts/multichain-runner.sh
@@ -233,7 +233,7 @@ run_main_logic() {
         -e POSTCHAIN_CONFIG=/config/config.0.properties \
         -e POSTCHAIN_BLOCKCHAIN_CONFIG=/build/manager.xml \
         -p $NODE_PORT:9870/tcp \
-        -p 127.0.0.1:$API_PORT:7740/tcp \
+        -p thedockerhost:$API_PORT:7740/tcp \
         registry.gitlab.com/chromaway/postchain-chromia/chromaway/chromia-server:${CHROMIA_NODE_VERSION} \
         run-node > ./multichain-postchain.log &
 
