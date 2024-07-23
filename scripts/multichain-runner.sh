c0e7907365f0759f64af639f730c5696ec5ae1bc
Vedran Krecl <vedran.krecl@chromaway.com>
2024-07-23 10:31:09 +0200

    amend

--- scripts/multichain-runner.sh
+++ scripts/multichain-runner.sh
@@ -235,8 +235,7 @@ run_main_logic() {
         -p $NODE_PORT:9870/tcp \
         --publish 127:0.0.1:$API_PORT:7740/tcp \
         registry.gitlab.com/chromaway/postchain-chromia/chromaway/chromia-server:${CHROMIA_NODE_VERSION} \
-        run-node
-        # run-node > ./multichain-postchain.log &
+        run-node > ./multichain-postchain.log &
 
     debug "Fetching manager chain BRID..."
     BRID=""
