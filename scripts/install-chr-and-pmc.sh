f222896ed89f9c375f826510985dc39dbf51d061
Vedran Krecl <vedran.krecl@chromaway.com>
2024-07-23 13:00:14 +0200

    fix prev

--- scripts/install-chr-and-pmc.sh
+++ scripts/install-chr-and-pmc.sh
@@ -7,5 +7,5 @@ ln -s /opt/chromaway/chr/bin/chr /bin/chr
 
 # install management-console-3.21.7-dist.tar.gz
 # wget -nv -O - https://gitlab.com/chromaway/core-tools/management-console/-/package_files/124654080/download | tar -C /opt/chromaway -xz
-wget -nv -O - https://gitlab.com/chromaway/core-tools/management-console/-/package_files/139267062/download | tar -C /opt/chromaway -xz
+wget -nv -O - https://gitlab.com/chromaway/core-tools/management-console/-/package_files/139267065/download | tar -C /opt/chromaway -xz
 ln -s /opt/chromaway/management-console/bin/pmc /bin/pmc
