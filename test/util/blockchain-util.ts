764050ad9fdc872ea993c23b1f78076ecc34e338
Vedran Krecl <vedran.krecl@chromaway.com>
2024-07-23 14:39:10 +0200

    amend

--- test/util/blockchain-util.ts
+++ test/util/blockchain-util.ts
@@ -144,6 +144,8 @@ export async function fetchBlockchains(
   >("get_blockchains", {
     include_inactive: false,
   });
+  console.log("DIRKABOG#################################");
+  console.log(result);
 
   const blockchains: { [key: string]: Blockchain } = {};
   result.forEach((blockchain) => {
