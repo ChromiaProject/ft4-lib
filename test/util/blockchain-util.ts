245228cbd11c4739c039f60b578288a362954754
Vedran Krecl <vedran.krecl@chromaway.com>
2024-07-24 09:29:46 +0200

    amend

--- test/util/blockchain-util.ts
+++ test/util/blockchain-util.ts
@@ -20,7 +20,6 @@ export async function createChromiaClientToMultichain(
   // const url = nodeUrl || process.env.TEST_NODE_URL || "http://127.0.0.1:7740";
 
   const url = nodeUrl || "http://thedockerhost:7740";
-  console.log("URL##########: ", url);
   return createClient({
     directoryNodeUrlPool: url,
     blockchainRid: blockchainRid.toString("hex"),
