393290ce1bb5122b298d384333303f9e35288032
Vedran Krecl <vedran.krecl@chromaway.com>
2024-07-26 01:50:56 +0200

    amend

--- scripts/multichain-runner.sh
+++ scripts/multichain-runner.sh
@@ -292,7 +292,8 @@ run_main_logic() {
     for chain_num in $(bash scripts/chain-numbers.sh $NUM_BLOCKCHAINS)
     do
         include_brids $chain_num
-
+        debug "xml config file:::::::::::::::::::::::::::::::"
+        cat rell/out/ft4_multichain_test_$chain_num.xml
         MULTICHAIN_DAPP_BRID=$(
             pmc blockchain add \
                 --quiet \
