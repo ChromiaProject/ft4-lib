2e5b097db481e95e469f064ce4616dce705514dc
Vedran Krecl <vedran.krecl@chromaway.com>
2024-07-31 11:11:44 +0200

    new runner

--- test_anchor.js
+++ test_anchor.js
@@ -45,8 +45,6 @@
 
     const latestBlock = await fetch(`${nodeUrl}/blocks/${dappBrid}?limit=1&txs=true`)
     console.log(`Latest block height ${(await latestBlock.json())[0].height}`);
-    // const latestBlock22 = await fetch(`${"http://localhost:7740"}/blocks/${dappBrid}?limit=1&txs=true`)
-    // console.log(`Latest222222 block height ${(await latestBlock22.json())[0].height}`);
     
     const response2 = await fetch(`${nodeUrl}/blocks/${dappBrid}/${lastAnchoredBlock}?txs=true`);
     const dappBlock = await response2.json();
