56e87583893f91e9326148bca44dd777a2354272
Francesco Santanché <francesco.santanche@chromaway.com>
2024-09-12 09:47:52 +0000

    Merged in FT4-582-relldocs-for-account-transfer (pull request #703)
    FT4-582 relldocs for account transfer
    
    * added relldocs
    
    * fixed comments
    
    * added relldocs
    
    * Merge remote-tracking branch 'origin/development' into FT4-582-relldocs-for-account-transfer
    
    * Merged in touch-up-relldocs (pull request #705)
    
    Touch up relldocs
    
    * changed returns to return
    
    * fixes to upgrade to 0.14.1
    
    * fix
    
    * rolled back 0.14.1
    
    * updated chr and pmc
    
    * updated jest test rell version
    
    * fixed tests
    
    * updated integration cli version
    
    * updated multichain tools versions
    
    * updated multichain rell version
    
    * added better explanation
    
    
    Approved-by: Vedran Krecl
    
    
    Approved-by: Vedran Krecl

--- doc/release-notes/rell/Unreleased.md
+++ doc/release-notes/rell/Unreleased.md
@@ -6,5 +6,6 @@
 
 ### Added ✅
 - Added a blacklist for operations that may not be used after an auth operation (`evm_auth` and `ft_auth`). Defined in the `chromia.yml` as `core.auth.auth_op_blacklisted_operations`, it works the same way as `evm_signatures_authorized_operations`, except the former is a list of operations that are not allowed, while the latter includes only allowed operations.
+- Added RellDocs for everything
 
 ### Fixed 🔧
--- scripts/install-chr-and-pmc.sh
+++ scripts/install-chr-and-pmc.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# install chromia-cli-0.20.1-dist.tar.gz
+# install chromia-cli-0.20.12-dist.tar.gz
 mkdir -p /opt/chromaway/chr
-wget -nv -O - https://gitlab.com/chromaway/core-tools/chromia-cli/-/package_files/131721774/download | tar -C /opt/chromaway/chr -xz
+wget -nv -O - https://gitlab.com/chromaway/core-tools/chromia-cli/-/package_files/145601751/download | tar -C /opt/chromaway/chr -xz
 ln -s /opt/chromaway/chr/bin/chr /bin/chr
 
-# install management-console-3.21.7-dist.tar.gz
-wget -nv -O - https://gitlab.com/chromaway/core-tools/management-console/-/package_files/124654080/download | tar -C /opt/chromaway -xz
+# install management-console-3.29.0-dist.tar.gz
+wget -nv -O - https://gitlab.com/chromaway/core-tools/management-console/-/package_files/146759772/download | tar -C /opt/chromaway -xz
 ln -s /opt/chromaway/management-console/bin/pmc /bin/pmc
